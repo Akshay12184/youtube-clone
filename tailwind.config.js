5086c79cb7b070477a56124ff0d785c141f28468
akshay dhondai <akshay.dhondai@gmail.com>
2023-12-15 08:50:28 +0100

    files added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Tailwind-CSS-746f35baacda-0ae288ac7f0a/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '18': '4.5rem',
+        '68': '16rem'
+      }
+    },
+  },
+  plugins: [],
+}
+
